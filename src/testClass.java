5088f01d6264f5376bb6b89add86e4b1d7409240
of63ecyl <martin.ullrich@fau.de>
2016-10-21 10:56:18 +0200

    Add Message

--- src/testClass.java
+++ src/testClass.java
@@ -3,6 +3,6 @@ public class testClass {
 	 public static void main (String[] args)
      {
            // Ausgabe Hello World!
-           System.out.println("Hello World!");
+           System.out.println("Hello World! This is test branch!");
      }
 }
